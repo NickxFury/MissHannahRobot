edfbd5cda0c6c0e9174cf9e4ea259e72bc04cfbf
Mr-Dark-Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-03-16 22:02:04 +0000

    lets go to plugins

--- MissHannahRobot/plugins/sql/cust_filters_sql.py
+++ MissHannahRobot/plugins/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
 
-from SaitamaRobot.modules.helper_funcs.msg_types import Types
-from SaitamaRobot.modules.sql import BASE, SESSION
+from SaitamaRobot.plugins.helper_funcs.msg_types import Types
+from SaitamaRobot.plugins.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
